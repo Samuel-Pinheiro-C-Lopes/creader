f0d8d0418bf6237642591bb6689a314975946c76
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2024-12-19 08:33:55 -0300

    Base structure of the function and auxiliaries functions provided

---
+++ CReader.c
@@ -0,0 +1,95 @@
+///////////////////////////////////
+
+#pragma region Header
+
+    #include "CReader.h"
+
+#pragma endregion
+
+///////////////////////////////////
+
+#pragma region FUNCTIONS
+
+    // Summary: Receives a format and a address for the target variable
+    // Parameters: <Format: string passing type of variable, %d, %f...> and <Target: pointer to the
+    // variable to be attributed with the input>
+    // Returns: Success or error, if there's any
+    int CRead(char* format, void* target)
+    {
+       return 1;
+    }
+
+#pragma endregion
+
+///////////////////////////////////
+
+#pragma region AUXILIARY FUNCTIONS
+
+    // Summary: Reads from stdin buffer until size is reached or 'enter' key is pressed
+    // Parameters: <target: pointer to the target to be filled with the input> and 
+    // <size: maximum size that the target must receive>
+    // Return: Success or error, if there were any
+    int StrReader(char *target, int size)
+    {
+        int result;
+        
+        // [...]
+        
+        return result;
+    }
+
+    // Summary: Tries to parse a string source to an int target
+    // Parameters: <source: string to be readed and parsed> and
+    // <target: integer target to be attributed based on the string source>
+    // Returns: Success or error, if there were any
+    int StrToInt(char* source, int* target)
+    {
+        int result;
+
+        // [...]
+
+        return result;
+    }
+
+    // Summary: Tries to parse a string source to a float target
+    // Parameters: <source: string to be readed and parsed> and
+    // <target: float target to be attributed based on the string source>
+    // Returns: Success or error, if there were any
+    int StrToFloat(char *source, float* target)
+    {
+        int result;
+
+        // [...]
+        
+        return result;
+    }
+
+    // Summary: Tries to parse a string source to a double target
+    // Parameters: <source: string to be readed and parsed> and 
+    // <target: double target to be attributed based on the string source>
+    // Returns: Success or error, if there were any
+    int StrToDouble(char *source, float *target)
+    {
+        int result;
+
+        // [...]
+        
+        return result;
+    }
+
+    // Summary: Tries to parse a string source to a char target
+    // Parameters: <source: string to be readed and parsed> and
+    // <target: char target to be attributed based on the string source>
+    // Returns: Succes or error, if there were any
+    int StrToChar(char *source, char* target)
+    {
+        int result;
+
+        // [...]
+        
+        return result;
+    }
+
+#pragma endregion
+
+///////////////////////////////////
---
+++ CReader.h
@@ -0,0 +1,64 @@
+///////////////////////////////////
+
+#pragma region DEPENDENCIES
+
+    #include <stdio.h>
+    #include <limits.h>
+
+#pragma endregion
+
+///////////////////////////////////
+
+#pragma region DEFINITIONS
+
+    // For now, consider the maximum size of a string readed to be 255
+    #define STR_MAX_SIZE (255)
+
+    // Results of funcions that succeded in the task provided
+    #define SUCCESS (1)
+
+    // Expected errors
+    #define INVALID_FORMAT (-10) // in case the <char* format> is invalid in <int CRead([...])>
+
+    // in case the string source is empty in any of the 
+    // <int StrTo[...]([...])> auxiliary functions
+    #define STR_EMPTY (-30) 
+
+    // in case the string source has any char that is invalid to the target in any of the 
+    // <int StrTo[...]([...])> auxiliary functions
+    #define INVALID_CHAR (-31) 
+
+#pragma endregion
+
+///////////////////////////////////
+
+#pragma region FUNCTIONS
+
+    // Main function for this project, reads based on format provided to the target variable
+    // in case of the format specifying a string, the maximum size is needed
+    int CRead(char* format, void* target);
+
+#pragma endregion
+
+///////////////////////////////////
+
+#pragma region AUXILIARY FUNCTIONS
+
+    // Reads from input until size of string is reached or 'enter' key is pressed
+    int StrReader(char* target, int size);
+    
+    // Tries to parse a string source to an int target
+    int StrToInt(char* source, int* target);
+
+    // Tries to parse a string source to a float target
+    int StrToFloat(char* source, float* target);
+    
+    // Tries to parse a string source to a double target
+    int StrToDouble(char* source, float* target);
+
+    // Tries to parse a string source to a char target
+    int StrToChar(char* source, char* target);
+    
+#pragma endregion
+
+///////////////////////////////////
